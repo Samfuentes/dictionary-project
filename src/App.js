5c8a90f8a6c53bc0a731b2839bf44b76cc214a6e
SamFuentes <sam.fuentesg@gmail.com>
2022-03-21 18:43:56 -0600

    Created dictionary component and search engine

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import "./App.css";
+import Dictionary from "./Dictionary.js";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
-      <h1>Hello</h1>
+      <div className="container">
+        <header className="App-header">Dictionary</header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <smaller>Code by Sam Fuentes, open-source on GitHub.</smaller>
+        </footer>
+      </div>
     </div>
   );
 }
