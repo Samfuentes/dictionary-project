a28a5083be5f8ba58eb5575e09c5e58766a3a899
SamFuentes <sam.fuentesg@gmail.com>
2022-04-04 20:25:42 -0500

    Added phonetic component

--- src/Meaning.js
+++ src/Meaning.js
@@ -2,7 +2,6 @@ import React from "react";
 import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
-  console.log(props.data);
   return (
     <div className="Meaning">
       <h3>{props.meaning.partOfSpeech}</h3>
@@ -11,18 +10,16 @@ export default function Meaning(props) {
         {props.meaning.definitions.map(function (definition, index) {
           return (
             <div key={index}>
-              <ul>
-                <li>
-                  {definition.definition}
-                  <br />
-                  <em>{definition.example}</em>
-                </li>
-              </ul>
-              <Synonyms synonyms={definition.synonyms} />
+              <p>
+                {definition.definition}
+                <br />
+                <em>{definition.example}</em>
+              </p>
             </div>
           );
         })}
       </div>
+      <Synonyms synonyms={props.meaning.synonyms} />
     </div>
   );
 }
--- src/Synonyms.js
+++ src/Synonyms.js
@@ -1,12 +1,15 @@
 import React from "react";
 
 export default function Synonyms(props) {
-  console.log(props.data);
   if (props.synonyms) {
     return (
       <div className="Synonyms">
         {props.synonyms.map(function (synonym, index) {
-          return <span key={index}>{synonym}</span>;
+          return (
+            <div>
+              <li key={index}>{synonym}</li>
+            </div>
+          );
         })}
       </div>
     );
